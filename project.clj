f796f1061887082631c052cbdcabce1bc64a8795
jem <michael.jerger@meissa-gmbh.de>
2019-11-23 12:48:43 +0100

    adjust deploy repos

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
 (defproject dda/cryogen-bootstrap "0.1.0-SNAPSHOT"
   :description "bootstrap theme for cryogen"
-  :license {:name "Apache2.0"}
+  :url "https://domaindrivenarchitecture.org"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring/ring-devel "1.7.1"]
                  [compojure "1.6.1"]
                  [ring-server "0.5.0"]
                  [dda/cryogen-core "0.1.53"]
                  [dda/cryogen-markdown "0.1.6"]]
-  :repositories [["clojars" :clojars]]
-  :deploy-repositories [["clojars" :clojars]])
+  :repositories [["snapshots" :clojars]
+                 ["releases" :clojars]]
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]])
