942188f056e850789ae0f0a545a51d390b4aa6ef
jem <michael.jerger@meissa-gmbh.de>
2020-02-22 15:44:47 +0100

    going to release

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [ring/ring-devel "1.7.1"]
                  [compojure "1.6.1"]
                  [ring-server "0.5.0"]
-                 [dda/cryogen-core "0.2.0-SNAPSHOT"]
+                 [dda/cryogen-core "0.2.0"]
                  [dda/cryogen-markdown "0.1.6"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
